da11eec9faf473dd2c025b8ad3634b6ef0936d20
Hiram Verdefronte <hiram.verdefronte@upa.edu.py>
2022-09-23 09:33:23 -0400

    categoria funcional

--- models/product.py
+++ models/product.py
@@ -50,39 +50,6 @@ class ProductModel(db.Model):
         for no_pk_key in ['descripcion', 'estado', 'nombre', 'precio', 'proveedor_id', 'categoria_id']:
             _assign_if_something(self, newdata, no_pk_key)
 
-class CatModel(db.Model):
-    __tablename__ = 'categoria'
-    id = db.Column(db.Integer, primary_key=True)
-    descripcion = db.Column(db.String)
-
-    def __init__(self, id, descripcion):
-        self.id = id
-        self.descripcion = descripcion
-
-
-    def json(self, depth =0):
-        json = {
-            'id': self.id,
-            'descripcion': self.descripcion
-        }
-
-        return json
-    
-    @classmethod
-    def find_by_id(cls, id):
-        return cls.query.filter_by(id=id).first()
-
-    def save_to_db(self):
-        db.session.add(self)
-        db.session.commit()
-
-    def delete_from_db(self):
-        db.session.delete(self)
-        db.session.commit()
-
-    def from_reqparse(self, newdata: Namespace):
-        for no_pk_key in ['description']:
-            _assign_if_something(self, newdata, no_pk_key)
 
 class ProdModel(db.Model):
     __tablename__ = 'proveedor'
